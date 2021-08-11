667e4c1577d97c2826ba06b67960e1f7fed88e18
yuliya-karimova <y.o.karimova@gmail.com>
2021-08-11 14:25:33 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+    const digits = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety','hundred'];
+    const digitsBig = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety','hundred'];
+    
+    const u = number % 10;
+    const d = Math.floor(number % 100 / 10);
+    const du = number % 100;
+    const h = Math.floor(number / 100);
+
+    const units = number === 0 ? 'zero' : du < 20 ? digits[du] : digits[u];
+    const dozens = du < 20 ? '' : u === 0 ? digitsBig[d] : `${digitsBig[d]} `
+    const hundreds = h === 0 ? '' : du === 0 ? `${digits[h]} hundred` : `${digits[h]} hundred `;
+    return `${hundreds}${dozens}${units}`;
 }
